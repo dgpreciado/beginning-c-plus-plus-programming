105376927f6b68864b95347b712db47414da9ed2
dgpreciado <37858944+dgpreciado@users.noreply.github.com>
2019-12-31 17:57:23 -0700

    section 10 done

--- section10/sec10_strings.cpp
+++ section10/sec10_strings.cpp
@@ -90,7 +90,7 @@ int main(){
 
     //strings_and_functions();
 
-    cout<<"----------------------------------------\n"<<endl;
+    /*cout<<"----------------------------------------\n"<<endl;
     string s1;
     string s2 {"Frank"};//Frank
     string s3 {s2};//Frank
@@ -158,7 +158,7 @@ int main(){
     sl+=" James";
     cout<<sl<<endl;
 
-    cout<<"\n------------------------STRING.LENGTH()----------------"<<endl;
+    cout<<"\n------------------------STRING.LENGTH()----------------"<<endl;*/
     /*
      * string s1;
      * cin >> s1; //user enters "Hello there"
@@ -172,11 +172,42 @@ int main(){
      * cout << s1 << endl;// This is
      *
      * */
+    std::string letters{};
 
+    std::cout << "Enter a string of letters so I can create a Letter Pyramid from it: ";
+    getline(std::cin, letters);
 
+    size_t num_letters = letters.length();
 
+    int position {0};
 
+    // for each letter in the string
+    for (char c: letters) {
 
+        size_t num_spaces = num_letters - position;
+        while (num_spaces > 0) {
+            std::cout << " ";
+            --num_spaces;
+        }
+
+        // Display in order up to the current character
+        for (size_t j=0; j < position; j++) {
+            std::cout << letters.at(j);
+        }
+
+        // Display the current 'center' character
+        std::cout << c;
+
+        // Display the remaining characters in reverse order
+        for (int j=position-1; j >=0; --j) {
+            // You can use this line to get rid of the size_t vs int warning if you want
+            auto k = static_cast<size_t>(j);
+            std::cout << letters.at(k);
+        }
+
+        std::cout << std::endl; // Don't forget the end line
+        ++position;
+    }
 
     return 0;
 }
