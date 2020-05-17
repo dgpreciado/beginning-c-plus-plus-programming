7e0e34e14eb9ad1afed65cec4a465fa7f46f168c
dgpreciado <37858944+dgpreciado@users.noreply.github.com>
2020-05-17 17:16:56 -0600

    226 live demo 2 done

---
+++ section19/sec19_226_readfile2.cpp
@@ -0,0 +1,25 @@
+//
+// Created by gabe on 5/17/20.
+//
+
+#include <iostream>
+#include <fstream>
+int main(){
+    std::ifstream in_file;
+    in_file.open("../section19/poem.txt");
+    if(!in_file){
+        std::cerr << "Problem opening file" << std::endl;
+        return 1;
+    }
+    /*std::string line {};
+    while (std::getline(in_file,line)){
+        std::cout << line << std::endl;
+    }
+    */
+    char c {};
+    while(in_file.get(c)){
+        std::cout<<c;
+    }
+    in_file.close();
+    return 0;
+}
--- section19/sec19_read_text_file_live_demo.cpp
+++ section19/sec19_read_text_file_live_demo.cpp
@@ -4,18 +4,34 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
-int main(){
+int main225(){
     std::ifstream in_file;
     std::string line;
-    in_file.open("../section19/test.txt");
+    in_file.open("../section19/test2.txt");
     if(!in_file){
         std::cerr << "Problem opening file" << std::endl;
         return 1;
     }
     std::cout << "file is good to go" << std::endl;
-    in_file >> line;
-    std::cout << line << std::endl;
+    //in_file >> line;
+    //std::cout << line << std::endl;
+    int num {};
+    double total {};
 
+    /*single line read*/
+    /*in_file>> line >> num >> total;
+    std::cout << line << std::endl;
+    std::cout << num << std::endl;
+    std::cout << total << std::endl;
+    */
+    /*multi line read*/
+    while (!in_file.eof()){
+        in_file>> line >> num >> total;
+        std::cout << std::left << std::setw(8) << line << std::setw(5) << num
+             << total << std::endl;
+    }
+    in_file.close();
     return 0;
 }
